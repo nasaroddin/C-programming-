03a4e3e7dea59684280855834a2bb22b4ebae9cd
nasaroddin <nasaroddin@eng.rizvi.edu.in>
2024-10-16 17:25:03 +0530

    Add files via upload

---
+++ Experiment1/exp1 task1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ printf("Full Name\t:Nasaroddin Salim Kazi \n");
+printf("Age\t\t:18\n");
+printf("class\t\t:FE\n");
+printf("Division\t:A\n");
+printf("UIN\t\t:241A040");
+
+ return 0;
+}
---
+++ Experiment1/exp1task2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+  int p,c,m,a;
+      printf("Enter Physics Marks:");
+      scanf("%d",&p);
+      printf("Enter chemistry Marks:");
+      scanf("%d",&c);
+      printf("Enter Mathematics Marks:");
+      scanf("%d",&m);
+      a=(p+c+m)/3;
+      printf("PCM Average=%d \n",a);
+if(a>=50)
+{
+    printf("You are Eligible for Admission");
+
+
+}
+else(a,50);
+printf("You are NOT Eligible for Admission ");
+
+return 0;
+
+
+
+
+
+}
+
