15348df9803af4db1e984d61bdc35d450584700f
nasaroddin <nasaroddin@eng.rizvi.edu.in>
2024-10-16 17:27:01 +0530

    Add files via upload

---
+++ Experiment2/exp2task1n.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("\t\t\t***Even Odd Finder***\n\n\n");
+
+    printf("Enter Number:");
+    scanf("%d",&num);
+
+    if (num%2==0)
+
+    {
+        printf("%d is Even",num);
+    }
+    else
+    {
+        printf("%d is odd",num);
+
+    }
+
+    return 0;
+}
